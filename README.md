1c113592da29b895acf48210f6cc865cf1ecf8f2
Imran Sultan <isultan030@gmail.com>
2022-12-10 20:28:31 -0600

    Add virial radius code

--- README.md
+++ README.md
@@ -25,9 +25,24 @@ replacing \<PATH TO IHT\> with the correct path.
 
 `posC` will be a 1D array of length 3 containing the (x,y,z) coordinates of the halo center. The halo center returned is in the same units as the `pdata['Coordinates']` array.
 
+### Virial Radius and Virial Mass
+
+    from iht import find_Rvir_SO
+    Rvir, Mvir = find_Rvir_SO(part, posC)
+
+`part` is a *dictionary of dictionaries* containing all of the particle data at a snapshot, i.e. the keys of `part` are integers $\in \{ 0,1,2,4,5 \}$ specifying the particle type (see FIRE Wiki); the values are dictionaries of the form `{'Coordinates':Nx3 array, 'Masses': 1D array of length N}`. 
+The units of `'Coordinates'` should be in physical kpc, and `'Masses'` should be in $10^{10} M_\odot$.
+
+The `part[0]` dictionary should also contain `part[0]['Omega_Matter']`, `part[0]['Omega_Lambda']`, `part[0]['HubbleParam']`, and `part[0]['Redshift']` (note that the first two of these parameters are named differently in FIRE-2 simulations).
+
+`posC` is a 1D array of length 3 containing the (x,y,z) coordinates of the halo center, in units of physical kpc.
+
+`Rvir` (virial radius calculated using spherical overdensity definition) will be returned in units of physical kpc; the virial mass `Mvir` (mass within a sphere of radius `Rvir` centered at `posC`) is returned in units $M_\odot$.
+
 ## Dependencies
 Python 3 is required. Running in a [conda](https://conda.io/projects/conda/en/latest/index.html) environment is recommended.
 
 The following packages are also required:
 - NumPy
 - Numba
+- Matplotlib
--- iht.py
+++ iht.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from numba import njit
 
 ### Halo centering ###
@@ -67,3 +68,85 @@ def halo_center_wrapper(pdata, shrinkpercent=2.5, minparticles=1000, initialradi
     coords = pdata['Coordinates']
     masses = pdata['Masses']
     return halo_center(coords, masses, shrinkpercent, minparticles, initialradiusfactor)[0]
+
+'''Finds virial radius (units physical kpc) given particle dict and halo center, using spherical overdensity definition.
+If `halo` and `snapnum` are defined, a plot of density vs. distance from halo center is saved.
+The virial mass (mass within a sphere of radius Rvir centered at posC) is also returned in units Msun.
+'''
+def find_Rvir_SO(part, posC=None, halo=None, snapnum=None):
+    if posC is None:
+        posC = part[0]['posC']
+
+    Masses = []
+    r = []
+    for ptype, p_i in part.items():
+        if not 'Masses' in p_i.keys(): #some simulations don't have ptype 5 (black holes)
+            print(f'{halo}: Masses not found for ptype {ptype} at snapshot {snapnum}')
+            continue
+        
+        Masses.append(p_i['Masses'])
+        
+        # position relative to center
+        p_i_CoordinatesRelative = p_i['Coordinates'] - posC
+
+        # distance from halo center
+        p_i_r = np.linalg.norm(p_i_CoordinatesRelative, axis=1)
+
+        r.append(p_i_r)
+    
+    Masses = np.concatenate(Masses)
+    r = np.concatenate(r)
+
+    idx = np.argsort(r)
+    Masses = Masses[idx]
+    r = r[idx]
+    Volume = 4/3 * np.pi * r**3 # Volume in units (physical kpc)^3
+
+    Masses = np.cumsum(Masses) * 1.e10 # Total mass in units Msun within sphere of radius r
+
+    with np.errstate(divide='ignore'): Density = Masses/Volume * 1.e9 # Density in units Msun/Mpc^3
+
+    if 'Omega0' in part[0]: #FIRE-2
+        OmegaM0, OmegaL0, hubble, z = part[0]['Omega0'], part[0]['OmegaLambda'], part[0]['HubbleParam'], part[0]['Redshift']
+    else: #FIRE-3
+        OmegaM0, OmegaL0, hubble, z = part[0]['Omega_Matter'], part[0]['Omega_Lambda'], part[0]['HubbleParam'], part[0]['Redshift']
+    rhovir = deltavir(OmegaM0, OmegaL0, z) * rhocritz(OmegaM0, OmegaL0, z) * hubble**2 # Virial density in units Msun/Mpc^3
+
+    if halo is not None:
+        plt.plot(r, Density)
+        plt.yscale('log')
+        plt.axhline(rhovir, label=r'$\Delta_{vir} \rho_{crit}$')
+        plt.xlim(0,300)
+        plt.xlabel('r (pkpc)')
+        plt.ylabel('Density (Msun/pMpc^3)')
+        plt.legend()
+        plt.savefig(f'density_{halo}_snapnum_{snapnum}.png')
+        plt.close()
+    
+    idx_vir = np.flatnonzero(Density <= rhovir)[0]
+    return r[idx_vir], Masses[idx_vir] # return Rvir in units physical kpc, and Mvir in units Msun
+    # simple linear interpolation with next closest point, and InterpolatedUnivariateSpline.roots() both seem to return approximately same Rvir as the 1 point method above.
+
+### COSMOLOGY CODE ###
+def scale_factor_to_redshift(a):
+    z = 1/a - 1
+    return z
+
+def Ez(OmegaM0, OmegaL0, z):
+    return ( OmegaM0*(1+z)**3 + OmegaL0 )**0.5
+
+def OmegaM(OmegaM0, OmegaL0, z):
+    return OmegaM0 * (1+z)**3 / Ez(OmegaM0, OmegaL0, z)**2
+
+def xz(OmegaM0, OmegaL0, z):
+    return OmegaM(OmegaM0, OmegaL0, z) - 1
+
+def deltavir(OmegaM0, OmegaL0, z):
+    '''Virial overdensity fitting function from Bryan & Norman (1998)'''
+    x = xz(OmegaM0, OmegaL0, z)
+    return 18*np.pi**2 + 82*x - 39*x**2
+
+def rhocritz(OmegaM0, OmegaL0, z):
+    '''Returns the critical density at z in units h^2 Msun/Mpc^3.'''
+    rhocrit0 = 2.77536627e11 #h^2 Msun/Mpc^3
+    return rhocrit0 * Ez(OmegaM0, OmegaL0, z)**2
